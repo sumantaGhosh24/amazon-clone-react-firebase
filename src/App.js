add7298ee5c09b8895b596b81493d981c0174411
sumanta ghosh <sumantaghosh5333@gmail.com>
2022-10-14 19:15:43 +0530

    complete

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, {useEffect} from "react";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+
+import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
+import Login from "./Login";
+
+function App() {
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>> ", authUser);
+
+      if (authUser) {
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+
+  return (
+    <div className="app">
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
